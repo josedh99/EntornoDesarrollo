ac41d327eb9a4d70580a039bea1f87c8d98aeaa0
josedh99 <115142082+josedh99@users.noreply.github.com>
2023-12-02 19:14:14 +0100

    V3

--- JuegoDePreguntasRespuestas/src/main/java/com/mycompany/juegodepreguntasrespuestas/JFrameJuego.java
+++ JuegoDePreguntasRespuestas/src/main/java/com/mycompany/juegodepreguntasrespuestas/JFrameJuego.java
@@ -26,21 +26,89 @@ public class JFrameJuego extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("preguntas");
+
+        jTextField1.setText("Escribe aqui tu respuesta");
+
+        jButton1.setText("Enviar");
+        jButton1.setEnabled(false);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Empezar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("0/0");
+
+        jLabel3.setText("JUEGO DE PREGUNTAS");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(55, 55, 55)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(jButton1)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 72, Short.MAX_VALUE)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(45, 45, 45)
+                            .addComponent(jButton2))
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 298, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 298, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(43, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel3)
+                .addGap(137, 137, 137))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(13, 13, 13)
+                .addComponent(jLabel3)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2)
+                    .addComponent(jLabel2))
+                .addContainerGap(18, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        JuegoDePreguntasRespuestas.empezarPasar();
+// TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        JuegoDePreguntasRespuestas.pregunata();
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +146,11 @@ public class JFrameJuego extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.JButton jButton1;
+    public javax.swing.JButton jButton2;
+    public javax.swing.JLabel jLabel1;
+    public javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    public javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
--- JuegoDePreguntasRespuestas/src/main/java/com/mycompany/juegodepreguntasrespuestas/JuegoDePreguntasRespuestas.java
+++ JuegoDePreguntasRespuestas/src/main/java/com/mycompany/juegodepreguntasrespuestas/JuegoDePreguntasRespuestas.java
@@ -4,14 +4,86 @@
 
 package com.mycompany.juegodepreguntasrespuestas;
 
+import java.util.Random;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 /**
  *
  * @author jose
  */
 public class JuegoDePreguntasRespuestas {
 
+    static JLabel pregunta = new JLabel();
+    static JTextField respuesta = new JTextField();
+    static JButton[] botones = new JButton[2];
+    static JLabel contador = new JLabel(); 
+    static String[] preguntas = {"¿Cuál es la capital de Francia?","¿En qué año se independizó Estados Unidos?","¿Cuántos continentes hay en el mundo?","¿Quién escribió \"Romeo y Julieta\"?","¿Cuál es el elemento más abundante en la corteza terrestre?","¿Cuál es el río más largo del mundo?","¿Qué planeta es conocido como \"el planeta rojo\"?","¿Cuál es la montaña más alta del mundo?","¿Quién fue el primer presidente de Estados Unidos?","¿En qué año se fundó la Organización de las Naciones Unidas (ONU)?","¿Cuál es el animal terrestre más grande?","¿Qué gas compone la mayor parte de la atmósfera de la Tierra?","¿Cuál es la velocidad de la luz en el vacío?","¿Quién pintó la Mona Lisa?","¿Cuál es la fórmula química del agua?"};
+    static String[] respuestas = {"Paris","1776","7","William Shakespeare","Oxigeno","El rio Amazonas","Marte","El Monte Everest","George Washington","1945","El elefante africano","Nitrogeno","300000 kilometros por segundo","Leonardo da Vinci","H2O"};
+    static String[] respuestas2 = {"Paris","1776","7","William Shakespeare","Oxigeno","Amazonas","Marte","Everest","George Washington","1945","elefante","Nitrogeno","300000 Km/s","Leonardo da Vinci","H2O"};
+
+    static int respuectaCorrecta = 0;
+            
     public static void main(String[] args) {
         JFrameJuego juego = new JFrameJuego();
+        pregunta = juego.jLabel1;
+        contador = juego.jLabel2;
+        respuesta = juego.jTextField1;
+        botones[0] = juego.jButton1;
+        botones[1] = juego.jButton2;
         juego.setVisible(true);
+    }
+    
+    public static void pregunata(){
+        if (respuesta.getText().equalsIgnoreCase(respuestas[respuectaCorrecta])||respuesta.getText().equalsIgnoreCase(respuestas2[respuectaCorrecta])) {
+            int indice=contador.getText().indexOf("/");
+            contador.setText((Integer.parseInt(contador.getText().substring(0, indice))+1)+contador.getText().substring( indice,contador.getText().length()));
+        }
+        {
+            int indice=contador.getText().indexOf("/");
+            contador.setText(contador.getText().substring(0, indice+1)+(Integer.parseInt(contador.getText().substring( indice+1,contador.getText().length()))+1));
+        }
+        
+        int numPreguntas=preguntas.length;
+        for(String i:preguntas){
+            numPreguntas=i==null?numPreguntas-1:numPreguntas;
+        }
+        
+        int numrandom;
+        do {
+            numrandom = new Random().nextInt(0, preguntas.length-1);
+        } while (preguntas[numrandom]==null&&numPreguntas>=2);
+        
+        if (numPreguntas==1) {
+            pregunta.setText("Ya no hay mas preguntas");
+            botones[0].setEnabled(false);
+            botones[1].setEnabled(false);
+            respuesta.setEnabled(false);
+        }else{
+            pregunta.setText(preguntas[numrandom]);
+        }
+        respuectaCorrecta=numrandom;
+        preguntas[numrandom]=null;
+        respuesta.setText("");
+    }
+    
+    public static void empezarPasar(){
+        if(botones[1].getText().equalsIgnoreCase("pasar")){
+            int numRan;
+            do {
+            numRan = new Random().nextInt(0, preguntas.length-1);
+            } while (preguntas[numRan]==null);
+            pregunta.setText(preguntas[numRan]);
+            respuectaCorrecta = numRan;
+        }else{
+            respuesta.setText("");
+            botones[1].setText("Pasar");
+            botones[0].setEnabled(true);
+            int numRan = new Random().nextInt(0, preguntas.length-1);
+            pregunta.setText(preguntas[numRan]);
+            respuestas[numRan]=null;
+            respuectaCorrecta = numRan;
+        }
     }
 }
